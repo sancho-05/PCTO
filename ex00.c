d5101de63ef003feee211a6a5edaa90e0ac417d8
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-20 12:36:01 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,17 @@
+//creare una variabile di tipo float, inizializzarla ad un valore a vostra scelta 
+// stampare la variabile 
+
+#include <stdio.h>
+int main()
+{
+   
+    float x = 4.2;
+    char a = 'A';
+    char l = 'L';
+    int b = 81; 
+
+    
+    printf("ho creato una variabile di tipo float con valore %f, il primo carattere %c è uguale al secondo carattere %c, ho creato una terza variabile int con valore %c/n", x,a,l,b);
+    
+     
+}
