de8074c613a1f9aa9cb6a077ff5c1de21434c23d
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-20 14:25:53 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+    printf("inserisci un numero");
+    scanf("%d", &x);
+    printf("x: %d\n", x);
+
+    printf ("inserisci un numero");
+    scanf("%d", &y);
+    printf ("x: %d\n" , y); 
+
+
+
+
+
+     
+}
