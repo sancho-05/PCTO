4163b8274a65b8fadb827c5774f80b8c5170cc8d
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-20 15:50:09 +0100

    Add files via upload

---
+++ ex05.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+int a;
+printf("dimmi la tua età");
+scanf("%d", &a); 
+
+if(a>18)
+{
+    printf("maggiorenne"); 
+}
+else
+{
+    printf("minorenne");
+}
+
+}
+
+
