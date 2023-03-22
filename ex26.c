d1397c38fc9b38df3248c3f8b459129a8c5b9c94
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-22 11:30:43 +0100

    Add files via upload

---
+++ ex26.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y = 0;
+    int z = 0;
+    int k;
+    while(y == 0)
+    {
+        printf("inserisci un numero di pezzi da aggiungere: ");
+        scanf("%d", &x);
+        if(x > 0)
+        {
+            z = z + x;
+            if(z > 50)
+            {
+                k = (z * 5) * (100 / 85);
+                printf("Il prezzo totale ammonta a: %d\n", k);
+            }
+            else
+            {
+                if(z > 30)
+                {
+                    k = (z * 5) * (100 / 90);
+                printf("Il prezzo totale ammonta a: %d\n", k);
+                }
+                else
+                {
+                    k = z * 5;
+                printf("Il prezzo totale ammonta a: %d\n", k);
+                }
+            }
+
+           
+        }
+        else
+        {
+            y = y - 1;
+            printf("Hai inserito il numero negativo %d, quindi il programma si è stoppato\n", x);
+        }
+        
+    }
+    
+    
+}
