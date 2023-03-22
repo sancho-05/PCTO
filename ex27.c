9e05e829928499bfacdca861eafd0a39fe4d9657
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-22 14:19:22 +0100

    Add files via upload

---
+++ ex27.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int potenza(int a, int b);
+{
+while (e>0)
+{
+    p=a*a;
+    b--;
+}
+
+
+
+}
+
+int main()
+{
+ int x= 2;
+ int y= 4;
+
+ int a= 3;
+ int b= 5;
+ 
+ int p= potenza(x,y);
+ printf("la potenza è %d\n", p);
+
+}
